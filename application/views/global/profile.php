f0f184ef0589bd33ead3fc70a978ab2c1bf678f6
Miptha Nrg <mipthanrg@gmail.com>
2020-11-10 21:20:03 +0700

    first commit

---
+++ application/views/global/profile.php
@@ -0,0 +1,50 @@
+<div class="row">
+	<div class="col m 12">
+		<h5>Profil Perpustakaan PUSTAKA.ID</h5>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col m6">
+		<div class="card-panel teal darken-2 white-text">
+			<h6><b>Visi</b></h6>
+			<p>
+			visi perpustakaan pada umumnya untuk mewujudkan masyarakat informasi atau masyarakat yang cerdas. 
+			<p>Untuk perpustakaan Kampus memiliki visi secara umum</p>
+			<p> menciptakan lulusan dan tamatan yang beriman dan bertakwa kepada Tuhan Yang Maha Esa
+			<p> berbudi luhur berakhlak mulia, cerdas, serta menguasai ilmu pengetahuan dan teknologi sebagai aset bangsa dan negara.
+			</p><br>
+			<h6><b>Misi</b></h6>
+			<li>
+			Meningkatkan kesadaran terhadap warisan budaya, apresiasi seni, dan hasil temuan ilmiah
+			</li>
+			<li>
+			Ikut serta dalam upaya mencerdaskan kehidupan bangsa dalam arti luas.
+			</li>
+			<li>
+			Menyediakan layanan informasi sesuai dengan kebutuhan pemakainya.
+			</li>
+			<li>
+			Mendukung pendidikan perorangan secara mandiri maupun pendidikan formal pada semua jenjang.
+			</li>
+			<li>
+			Memberikan kesempatan atau stimulasi bagi pengembangan kreativitas dan imajinasi pribadi maupun masyarakat.
+			</li>
+		</div>
+	</div>
+    <div class="col m6">
+        <div class="card-panel teal darken-2 white-text">
+            <h6><b>Identitas Perpustakaan</b></h6><hr>
+            <p>
+				PUSTAKA.ID :</p>
+				<p>Nama : PUSTAKA.ID</p>
+				<p>Alamat : Jl.Aria Santika , Karawaci,Tangerang</p>
+				<p>Kepala Perpustakaan : Anisa Putri</p>
+				<p>Admin Perpustakaan : Miptha Nrg</p>
+				<p>Petugas Perpustakaan : Miptha</p>
+
+
+            </p>
+        </div>
+    </div>
+</div>
